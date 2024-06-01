aab847ea70663da09cec2fcad9caf2b1a3dd5cec
Mohit Singh <xyzmohit.singh@gmail.com>
2024-06-01 13:00:58 +0530

    rename

--- app/src/main/java/org/example/User.java
+++ app/src/main/java/org/example/User.java
@@ -2,15 +2,10 @@ package org.example;
 
 import java.util.List;
 
-public class App {
-    public static void main(String[] args) {
-
-    }
-    public class user {
+public class User {
         private String name;
         private String password;
         private String hashpassword;
         private List<Ticket> ticketsBooked;
         private String userID;
-    }
 }
