4eb22148137003407827942eb19d5974f54a6fab
Mohit Singh <xyzmohit.singh@gmail.com>
2024-06-01 12:58:19 +0530

    gradle init

---
+++ app/src/main/java/org/example/App.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import java.util.List;
+
+public class App {
+    public static void main(String[] args) {
+
+    }
+    public class user {
+        private String name;
+        private String password;
+        private String hashpassword;
+        private List<Ticket> ticketsBooked;
+        private String userID;
+    }
+}
