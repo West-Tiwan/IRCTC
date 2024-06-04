8945b232cb78a805240b084902856b605701ced5
Mohit Singh <xyzmohit.singh@gmail.com>
2024-06-04 22:17:16 +0530

    getters and setters

--- app/src/main/java/ticket/booking/entities/User.java
+++ app/src/main/java/ticket/booking/entities/User.java
@@ -4,8 +4,67 @@ import java.util.List;
 
 public class User {
         private String name;
+
         private String password;
+
         private String hashpassword;
+
         private List<Ticket> ticketsBooked;
+
         private String userId;
+
+        public User(String name, String password, String hashpassword, List<Ticket> ticketsBooked, String userId) {
+                this.name = name;
+                this.password = password;
+                this.hashpassword = hashpassword;
+                this.ticketsBooked = ticketsBooked;
+                this.userId = userId;
+        }
+
+        public User(){}
+
+        public String getName(){
+                return name;
+        }
+
+        public String getPassword(){
+                return password;
+        }
+
+        public String getHashpassword(){
+                return hashpassword;
+        }
+        public List<Ticket> getTicketsBooked(){
+                return ticketsBooked;
+        }
+
+        public String getUserId(){
+                return userId;
+        }
+
+        public void setName(String name){
+                this.name = name;
+        }
+
+        public void setPassword(String password){
+                this.password = password;
+        }
+
+        public void setHashpassword(String hashpassword){
+                this.hashpassword = hashpassword;
+        }
+
+        public void setTicketsBooked(List<Ticket> ticketsBooked){
+                this.ticketsBooked = ticketsBooked;
+        }
+
+        public void setUserId(String userId){
+                this.userId = userId;
+        }
+
+        public void printTickets(){
+                for (int i = 0; i < ticketsBooked.size(); i++) {
+                        System.out.println(ticketsBooked.get(i).getTicketInfo());
+                }
+        }
 }
